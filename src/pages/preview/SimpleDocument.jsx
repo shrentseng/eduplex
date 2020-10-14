3df10684d12d78f8badf58cf521e5698403a24a3
Oscar Cheng <oscarcheng105@gmail.com>
2020-10-14 14:12:10 +0800

    doc preview update

---
+++ src/pages/preview/SimpleDocument.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Typography } from '@material-ui/core'
+import SmallDoc from '../../assets/SmallDoc.svg'
+
+const useStyles = makeStyles(() => ({
+    root:{
+        margin: "0.5rem",
+    },
+    paper:{
+        display: "grid",
+        gridTemplateColumns:"20%",
+    },
+    image:{
+        margin:"0.5rem",
+        gridColumnStart:"1",
+        gridColumnEnd:"2",
+    },
+    title:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        gridColumnStart:"2",
+        gridColumnEnd:"3",
+    }
+}))
+const SimpleDocument = (props) => {
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+            <Paper variant="outlined" className={classes.paper}>
+                <img className={classes.image} src={SmallDoc}/>
+                <Typography className={classes.title}>{props.name}</Typography>
+            </Paper> 
+        </div>
+    )
+}
+
+export default SimpleDocument;
