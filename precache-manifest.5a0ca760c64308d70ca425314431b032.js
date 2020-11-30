5b5ba95d8132fa7fe2bbe710a6c31631f9980493
Shren Tseng <lspss94076@gmail.com>
2020-11-29 22:11:43 -0800

    Updates

---
+++ precache-manifest.5a0ca760c64308d70ca425314431b032.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061fa40aff1617235ec9da8b0f18bdf1",
+    "url": "/eduplex/index.html"
+  },
+  {
+    "revision": "50adfa2257b4c3fa1855",
+    "url": "/eduplex/static/css/main.5f3c732e.chunk.css"
+  },
+  {
+    "revision": "b5ddcf67de100ca2253b",
+    "url": "/eduplex/static/js/2.4fc3eebc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/eduplex/static/js/2.4fc3eebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50adfa2257b4c3fa1855",
+    "url": "/eduplex/static/js/main.1472072f.chunk.js"
+  },
+  {
+    "revision": "23307447783653eea91f",
+    "url": "/eduplex/static/js/runtime-main.258dca11.js"
+  },
+  {
+    "revision": "bb35baa02c44a18933ef372e5b19b5d9",
+    "url": "/eduplex/static/media/add-post.bb35baa0.svg"
+  },
+  {
+    "revision": "9df4abc541d7c315d6f722fd4a97f9bc",
+    "url": "/eduplex/static/media/anonymous.9df4abc5.svg"
+  },
+  {
+    "revision": "5087c9cf1ad3a54354906cbaf18a3c7b",
+    "url": "/eduplex/static/media/avatar.5087c9cf.svg"
+  },
+  {
+    "revision": "c0c4c07c9eb3f0b808dd37abbe7b15d7",
+    "url": "/eduplex/static/media/bookmark.c0c4c07c.svg"
+  },
+  {
+    "revision": "6c02305657843663b4f624ac28170afd",
+    "url": "/eduplex/static/media/comment.6c023056.svg"
+  },
+  {
+    "revision": "a9aef12477d3e769b4e9abaeffeb2711",
+    "url": "/eduplex/static/media/create-course.a9aef124.svg"
+  },
+  {
+    "revision": "bceff42a34b2b392e622a62a360b9382",
+    "url": "/eduplex/static/media/dislike.bceff42a.svg"
+  },
+  {
+    "revision": "6d87e694cce9679c93c9585a342fbaec",
+    "url": "/eduplex/static/media/disliked.6d87e694.svg"
+  },
+  {
+    "revision": "ce98abedb3f30290b64378a4b1a92de9",
+    "url": "/eduplex/static/media/document.ce98abed.svg"
+  },
+  {
+    "revision": "7f4ec59ee2d2303ace79e436e7f5e2a6",
+    "url": "/eduplex/static/media/dollar.7f4ec59e.svg"
+  },
+  {
+    "revision": "b13107b04815cf012ca35a3659af23f5",
+    "url": "/eduplex/static/media/download.b13107b0.svg"
+  },
+  {
+    "revision": "d9cee274af5ae9f400bbd833e87e8f69",
+    "url": "/eduplex/static/media/eduPlex.d9cee274.svg"
+  },
+  {
+    "revision": "f4207ef34ff21ebaac5bfc4ba171f40d",
+    "url": "/eduplex/static/media/enter.f4207ef3.svg"
+  },
+  {
+    "revision": "0e86d97d8145883c5ef4bf676329ff74",
+    "url": "/eduplex/static/media/epimg.0e86d97d.svg"
+  },
+  {
+    "revision": "e0df80704d137ced805dff3113bfa193",
+    "url": "/eduplex/static/media/exclude.e0df8070.svg"
+  },
+  {
+    "revision": "f9112959b3063280992205cbbadc906c",
+    "url": "/eduplex/static/media/help.f9112959.svg"
+  },
+  {
+    "revision": "3e8e6fda0d78e4e0d20161f37b956198",
+    "url": "/eduplex/static/media/like.3e8e6fda.svg"
+  },
+  {
+    "revision": "69f85512b01827af02228416056ae143",
+    "url": "/eduplex/static/media/liked.69f85512.svg"
+  },
+  {
+    "revision": "0b04c6d0e30f30327dc1834be55cba91",
+    "url": "/eduplex/static/media/myCourses.0b04c6d0.svg"
+  },
+  {
+    "revision": "a52d2a4b197ae75fb11a648386ed166f",
+    "url": "/eduplex/static/media/myCoursesActive.a52d2a4b.svg"
+  },
+  {
+    "revision": "de556069a8afea46c6fe479c47ee0735",
+    "url": "/eduplex/static/media/newsfeed.de556069.svg"
+  },
+  {
+    "revision": "2af389b2ab1f93df7af480c0200245dc",
+    "url": "/eduplex/static/media/newsfeedActive.2af389b2.svg"
+  },
+  {
+    "revision": "aa44f8d7e32b3c0c18a13403e14ca887",
+    "url": "/eduplex/static/media/receive-comment.aa44f8d7.svg"
+  },
+  {
+    "revision": "51fea8f675a02d16255f0b58eb32f1ac",
+    "url": "/eduplex/static/media/receive-download.51fea8f6.svg"
+  },
+  {
+    "revision": "58ca1d3d9093f4421c3a439030e745e8",
+    "url": "/eduplex/static/media/receive-upvote.58ca1d3d.svg"
+  },
+  {
+    "revision": "bfe6656a1bf235a8a4ca9265a96b0b0b",
+    "url": "/eduplex/static/media/refer-friend.bfe6656a.svg"
+  },
+  {
+    "revision": "7f0787054278d6415e31427f1f8677a1",
+    "url": "/eduplex/static/media/reward-icon.7f078705.svg"
+  },
+  {
+    "revision": "67bdfb3989629ac51e8cbb0b37857c76",
+    "url": "/eduplex/static/media/share.67bdfb39.svg"
+  },
+  {
+    "revision": "90510042cfa5ae526b2741173edf2a5e",
+    "url": "/eduplex/static/media/smallDoc.90510042.svg"
+  },
+  {
+    "revision": "be55c8fcbbd10277fcb8418b1a2e9a6f",
+    "url": "/eduplex/static/media/trophy.be55c8fc.svg"
+  },
+  {
+    "revision": "13e2f280eae63a464071aff0a44ebec4",
+    "url": "/eduplex/static/media/trophy1.13e2f280.svg"
+  },
+  {
+    "revision": "23947a7df03943b48c9669518616536a",
+    "url": "/eduplex/static/media/trophy2.23947a7d.svg"
+  },
+  {
+    "revision": "33ef4969dab6044529d89b9bac88c81a",
+    "url": "/eduplex/static/media/trophy3.33ef4969.svg"
+  },
+  {
+    "revision": "e59a2774e08cf7ccdea159a976a1cb34",
+    "url": "/eduplex/static/media/trophy4.e59a2774.svg"
+  },
+  {
+    "revision": "e59a2774e08cf7ccdea159a976a1cb34",
+    "url": "/eduplex/static/media/trophy5.e59a2774.svg"
+  },
+  {
+    "revision": "f85951cb1b04193c934d73075e96becb",
+    "url": "/eduplex/static/media/upload-doc.f85951cb.svg"
+  },
+  {
+    "revision": "9cf98c3b84dacbb1a24d494c698f0513",
+    "url": "/eduplex/static/media/upload.9cf98c3b.svg"
+  }
+]);
